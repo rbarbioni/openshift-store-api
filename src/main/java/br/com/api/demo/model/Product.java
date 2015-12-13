8ac184f8d2703d59b171533dc5a3d7f35d665d3b
Renan Barbioni <renanbarbioni@gmail.com>
2015-12-13 10:56:16 -0200

    ajusts

--- src/main/java/br/com/api/demo/model/Product.java
+++ src/main/java/br/com/api/demo/model/Product.java
@@ -1,16 +1,35 @@
 package br.com.api.demo.model;
 
-import javax.persistence.Entity;
+import java.util.Date;
 
-import br.com.rbarbioni.application.core.models.DomainEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created by root on 10/12/15.
  */
+@SuppressWarnings("unused")
 @Entity
-public class Product extends DomainEntity {
+public class Product {
 	
 	private static final long serialVersionUID = -2142641923557789412L;
+	
+	@Id
+	@GeneratedValue ( strategy = GenerationType.AUTO )
+	@Column ( name = "id", nullable=false )
+	private Long id;
+	
+    @Column(name = "uuid", nullable = false, updatable = false)
+    private String uuid = java.util.UUID.randomUUID().toString();	
+    
+	@Column ( name = "creation_date", nullable = false )
+    private Date creationDate = new Date();
+	
+	@Column ( name = "active", nullable=false )
+    private boolean active = true;
 	
 	private String name;
 
@@ -21,7 +40,36 @@ public class Product extends DomainEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO Product (active, creation_date, uuid, name) VALUES (1, '2015-01-01 15:22:40.997', '1', 'Galaxy S');
-INSERT INTO Product (active, creation_date, uuid, name) VALUES (1, '2015-01-01 15:22:40.997', '2', 'Galaxy S2');
-INSERT INTO Product (active, creation_date, uuid, name) VALUES (1, '2015-01-01 15:22:40.997', '3', 'Galaxy S3');
-INSERT INTO Product (active, creation_date, uuid, name) VALUES (1, '2015-01-01 15:22:40.997', '4', 'Galaxy S4');
-INSERT INTO Product (active, creation_date, uuid, name) VALUES (1, '2015-01-01 15:22:40.997', '5', 'Galaxy S5');
-INSERT INTO Product (active, creation_date, uuid, name) VALUES (1, '2015-01-01 15:22:40.997', '6', 'Galaxy GranDuos');
+INSERT INTO Product (id, active, creation_date, uuid, name) VALUES (1, 1, '2015-01-01 15:22:40.997', '1', 'Galaxy S');
+INSERT INTO Product (id, active, creation_date, uuid, name) VALUES (2, 1, '2015-01-01 15:22:40.997', '2', 'Galaxy S2');
+INSERT INTO Product (id, active, creation_date, uuid, name) VALUES (3, 1, '2015-01-01 15:22:40.997', '3', 'Galaxy S3');
+INSERT INTO Product (id, active, creation_date, uuid, name) VALUES (4, 1, '2015-01-01 15:22:40.997', '4', 'Galaxy S4');
+INSERT INTO Product (id, active, creation_date, uuid, name) VALUES (5, 1, '2015-01-01 15:22:40.997', '5', 'Galaxy S5');
+INSERT INTO Product (id, active, creation_date, uuid, name) VALUES (6, 1, '2015-01-01 15:22:40.997', '6', 'Galaxy GranDuos');
